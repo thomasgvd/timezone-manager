69985ba1d876a7e99bed9da6d972fc6579bcc3a7
Thomas Gervraud <thomasgervraud96@hotmail.fr>
2020-11-30 16:44:44 +0100

    First commit

---
+++ src/main/java/com/thomasgvd/joethebro/JoeTheBroApplication.java
@@ -0,0 +1,13 @@
+package com.thomasgvd.joethebro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeTheBroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeTheBroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomasgvd/joethebro/JoeTheBroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasgvd.joethebro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoeTheBroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
